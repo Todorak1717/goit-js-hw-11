5d725502c34c8073db874877673f59aa5a1a5d73
Todorak1717 <155604203+Todorak1717@users.noreply.github.com>
2024-06-30 00:19:04 +0000

    Deploying to gh-pages from @ Todorak1717/goit-js-hw-11@a5d016b09760adae6520336740767e793082055c 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,29 @@
+import{S as u,i as m}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();function p(r){const s="https://pixabay.com",i="/api/",o=new URLSearchParams({key:"44700012-753277454e0fe08e62d6e3109",q:`${r}`,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${s}${i}?${o}`;return fetch(e).then(t=>t.json()).catch(t=>{console.log(t)})}function f({webformatURL:r,largeImageURL:s,tags:i,likes:o,views:e,comments:t,downloads:a}){return` <li class="album-item">
+              <a class="album-link" href="${s}">
+              <img
+                class="album-img"
+                src="${r}"
+                alt="${i}"
+              />
+              <ul class="statistics-list">
+                <li class="statistics-item text-marg-1">
+                  <p class="statistics-text">Likes</p>
+                  <p>${o}</p>
+                </li>
+                <li class="statistics-item text-marg-2">
+                  <p class="statistics-text">Views</p>
+                  <p>${e}</p>
+                </li>
+                <li class="statistics-item text-marg-3">
+                  <p class="statistics-text">Comments</p>
+                  <p>${t}</p>
+                </li>
+                <li class="statistics-item">
+                  <p class="statistics-text">Downloads</p>
+                  <p>${a}</p>
+                </li>
+              </ul>
+              </a>
+            </li>`}function d(r){return r.map(f).join(`
+`)}const l=document.querySelector(".js-form"),c=document.querySelector(".js-album-list"),n=document.querySelector(".js-loader"),h=new u(".album-list a",{captionsData:"alt",captionDelay:250});l.addEventListener("submit",r=>{r.preventDefault(),c.innerHTML="";const s=r.target.elements.text.value.trim();s!==""&&(g(),p(s).then(i=>{i.hits.length===0?m.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",timeout:2e3}):(c.innerHTML=d(i.hits),h.refresh())}).catch(i=>{console.log(i)}).finally(()=>{y()})),l.reset()});function g(){n.classList.remove("hidden")}function y(){n.classList.add("hidden")}
+//# sourceMappingURL=commonHelpers.js.map
